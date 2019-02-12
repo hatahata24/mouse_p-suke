2d3c29ad0fd8107faf8281e27b26151a2dc1e003
Atsushi Hatanaka <walkman.ha@gmail.com>
2019-02-12 20:58:04 +0900

    HALF&R&L&180 clear

--- inc/params.h
+++ inc/params.h
@@ -17,18 +17,22 @@
 ------------------------------------------------------------*/
 
 #define WHEEL_DIAMETER		54
+#define WHEEL_DISTANCE		88
+
 #define ROUND_STEP			400
 #define ONE_STEP			(WHEEL_DIAMETER*M_PI/ROUND_STEP)
+
+#define SEC_HALF			90
 
 
 /*------------------------------------------------------------
 		走行系
 ------------------------------------------------------------*/
 //----走行パルス関連----
-#define PULSE_SEC_HALF		90/ONE_STEP //206				//半区画走行用パルス。2倍すると1区画分に 208
-#define PULSE_ROT_R90 		158//152//158		//右90度回転用パルス数 145
-#define	PULSE_ROT_L90 		158//152//158		//左90度回転用パルス数 145
-#define PULSE_ROT_180 		304//316		//180度回転用パルス数 289
+#define PULSE_SEC_HALF		SEC_HALF/ONE_STEP //206				//半区画走行用パルス。2倍すると1区画分に 208
+#define PULSE_ROT_R90 		WHEEL_DISTANCE*M_PI/4/ONE_STEP //158		//右90度回転用パルス数 145
+#define	PULSE_ROT_L90 		WHEEL_DISTANCE*M_PI/4/ONE_STEP//158		//左90度回転用パルス数 145
+#define PULSE_ROT_180 		WHEEL_DISTANCE*M_PI/2/ONE_STEP//304//316		//180度回転用パルス数 289
 #define PULSE_SETPOS_BACK 	200//250,300	//後ろ壁に当てるために下がるパルス数
 #define PULSE_SETPOS_SET 	108//100//92	//後ろ壁から中央までのパルス数
 
--- src/drive.c
+++ src/drive.c
@@ -593,8 +593,10 @@ void rotate_R90(void){
 	drive_set_dir(ROTATE_R);								//右に旋回するようモータの回転方向を設定
 	//drive_wait();
 	//driveC(PULSE_ROT_R90);									//デフォルトインターバルで指定パルス分回転。回転後に停止する
-	driveA(PULSE_ROT_R90*0.48);
-	driveD(PULSE_ROT_R90*0.48);
+	//driveA(PULSE_ROT_R90*0.48);
+	//driveD(PULSE_ROT_R90*0.48);
+	driveA(PULSE_ROT_R90*0.5);
+	driveD(PULSE_ROT_R90*0.5);
 	//drive_wait();
 	drive_set_dir(FORWARD);									//前進するようにモータの回転方向を設定
 }
@@ -611,8 +613,10 @@ void rotate_L90(void){
 	drive_set_dir(ROTATE_L);								//左に旋回するようモータの回転方向を設定
 	//drive_wait();
 	//driveC(PULSE_ROT_L90);									//デフォルトインターバルで指定パルス分回転。回転後に停止する
-	driveA(PULSE_ROT_L90*0.48);
-	driveD(PULSE_ROT_L90*0.48);
+	//driveA(PULSE_ROT_L90*0.48);
+	//driveD(PULSE_ROT_L90*0.48);
+	driveA(PULSE_ROT_L90*0.5);
+	driveD(PULSE_ROT_L90*0.5);
 	//drive_wait();
 	drive_set_dir(FORWARD);									//前進するようにモータの回転方向を設定
 }
